ad5908c6fd63b787b9b79853e7d16bd03cecb010
AnpabeltTrahJavala <patrapembinaan@gmail.com>
2023-12-07 04:49:38 +0700

    test modul 5

---
+++ belajar-laravel-9/resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+@section('berand')
+active
+@endsection
+@section('content')
+<div class="container">
+    <div class="row pb-5">
+        <div class="col-6">
+            <h1 class="text-light">Selamat Datang di Modul 5 WAD2023</h1>
+        </div>
+    </div>
+</div>
+@endsection
